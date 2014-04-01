f1a359a8fbb295ae3b3667eba745165a038d563a
Mike Burns <mgburns@bu.edu>
2014-04-01 15:25:41 -0400

    Update default settings

--- settings.py
+++ settings.py
@@ -3,3 +3,19 @@
 # Course-specifc grader configuration goes here
 # Loaded by ``bux_grader_framework`` at run time
 #
+
+XQUEUE_QUEUE = ""
+XQUEUE_URL = "http://localhost:18040"
+XQUEUE_USER = "lms"
+XQUEUE_PASSWORD = "password"
+XQUEUE_TIMEOUT = 10
+
+RABBITMQ_USER = "guest"
+RABBITMQ_PASSWORD = "guest"
+RABBITMQ_HOST = "localhost"
+RABBITMQ_PORT = 5672
+RABBITMQ_VHOST = "/"
+
+EVALUATOR_MODULES = {
+    "dummy_grader"
+}
